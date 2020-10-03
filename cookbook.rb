c78d7453576e5551c82aeb5f43722fe3e8ed0b1f
tmcamillo <imeio.tmc@gmail.com>
2020-10-03 16:37:34 -0300

    estrutura condicionais

--- cookbook.rb
+++ cookbook.rb
@@ -1,18 +1,36 @@
 begin
-    puts "Bem vindo ao cookbook, sua rede social de receitas!"
-    receitas = []
+	puts "Bem vindo ao cookbook, sua rede social de receitas!"
+	receitas = []
 
-    while(true) do
-        puts "Digite o nome da receita"
-        nome = gets.chomp()
-        receitas << nome
-        puts
-        puts "Receita #{nome} cadastrada com sucesso!"
-        puts "============= Receitas Cadastradas ============="
-        puts
-        puts receita
-        # receitas.each do |receita|
-        #     puts receita
-        # end
-    end
+	puts "[1] Cadastrar uma receita"
+	puts "[2] Ver todas as receitas"
+	puts "[3] Sair"
+
+	print "Escolha uma opção: " 
+	opcao = gets.to_i() #converte para numero
+
+	while(opcao != 3) do
+		if(opcao == 1)
+			puts "Digite o nome da receita"
+			nome = gets.chomp()
+			receitas << nome
+			puts
+			puts "Receita #{nome} cadastrada com sucesso!"
+			puts
+		elsif(opcao == 2)
+			puts "============= Receitas Cadastradas ============="
+			puts receitas
+			puts
+		else
+			puts "opcao invalida"
+		end
+		puts "[1] Cadastrar uma receita"
+		puts "[2] Ver todas as receitas"
+		puts "[3] Sair"
+
+		print "Escolha uma opção: " 
+		opcao = gets.to_i() #converte para numero
+	end
+
+	puts "obrigadao por usar o cookbook"
 end
