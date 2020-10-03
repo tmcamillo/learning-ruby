7b3b92ff8969f3cfa16a1ae74ef0a89346fe787f
tmcamillo <imeio.tmc@gmail.com>
2020-10-03 18:49:26 -0300

    refatoracao e boas praticas

--- cookbook.rb
+++ cookbook.rb
@@ -1,35 +1,59 @@
 begin
-	puts "Bem vindo ao cookbook, sua rede social de receitas!"
-	receitas = []
 
-	puts "[1] Cadastrar uma receita"
-	puts "[2] Ver todas as receitas"
-	puts "[3] Sair"
+INSERIR_RECEITA = 1	
+VISUALIZAR_RECEITAS = 2
+SAIR = 3
 
-	print "Escolha uma opção: " 
-	opcao = gets.to_i() #converte para numero
 
-	while(opcao != 3) do
-		if(opcao == 1)
-			puts "Digite o nome da receita"
-			nome = gets.chomp()
-			receitas << nome
-			puts
-			puts "Receita #{nome} cadastrada com sucesso!"
-			puts
-		elsif(opcao == 2)
-			puts "============= Receitas Cadastradas ============="
-			puts receitas
-			puts
-		else
-			puts "opcao invalida"
-		end
-		puts "[1] Cadastrar uma receita"
-		puts "[2] Ver todas as receitas"
-		puts "[3] Sair"
+	def bem_vindo()
+		puts "Bem vindo ao cookbook, sua rede social de receitas!"
+	end
+
+	def menu()
+		puts "[#{INSERIR_RECEITA}] Cadastrar uma receita"
+		puts "[#{VISUALIZAR_RECEITAS}] Ver todas as receitas"
+		puts "[#{SAIR}] Sair"
 
 		print "Escolha uma opção: " 
-		opcao = gets.to_i() #converte para numero
+		  gets.to_i()
+	end
+
+	def inserir_receita()
+		puts "Digite o nome da receita"
+		nome = gets.chomp()
+		puts "Agora, digite o tipo da receita"
+		tipo = gets.chomp()
+		puts
+		puts "Receita #{nome} cadastrada com sucesso!"
+		return {nome: nome, tipo: tipo}
+	end
+
+	def imprimir_receitas(re)
+		puts "============= Receitas Cadastradas ============="
+		re.each do |receita|
+			puts "#{receita[:nome]} - #{receita[:tipo]}"
+			puts
+		end
+		if re.empty?
+			puts "Nenhuma receita ainda cadastrada"
+		end
+	end
+	
+	bem_vindo()
+	receitas = []
+	opcao = menu()
+
+	loop do
+		if(opcao == INSERIR_RECEITA)
+			receitas << inserir_receita()
+		elsif(opcao == VISUALIZAR_RECEITAS)
+			imprimir_receitas(receitas)
+		elsif(opcao == SAIR)
+			break
+		else	
+			puts "opcao invalida"
+		end
+		opcao = menu()
 	end
 
 	puts "obrigadao por usar o cookbook"
